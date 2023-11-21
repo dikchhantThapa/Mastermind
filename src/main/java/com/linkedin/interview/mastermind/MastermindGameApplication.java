efe91b9680fd9144d626b2775f8f206b713593bd
dikchhantThapa <dikchhant_thapa@yahoo.com>
2023-11-20 22:42:08 -0600

    Initial commit

---
+++ src/main/java/com/linkedin/interview/mastermind/MastermindGameApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.interview.mastermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/interview/mastermind/MastermindGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.interview.mastermind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
